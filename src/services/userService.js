5c61e6e4c57e6d9e468181fdfb886efe0606c626
Thomas Dutouquet <140429626+tdutouquet@users.noreply.github.com>
2024-04-30 21:23:54 +0200

    Fixed cookie issue

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const client = axios.create({
     baseURL: 'http://localhost:8000/api',
+    withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
